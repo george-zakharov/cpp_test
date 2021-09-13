ab66f7389872198b9ca926fd733dc12742c9c451
George Zakharov <george_zakharov@mail.ru>
2021-09-13 21:33:43 +0300

    Strings play

--- test.cpp
+++ test.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     int x, y, sum;
-    cout << "Hello User!" << endl;
+    string hello = "Hello User!";
+    string fullName;
+
+    cout << hello << endl;
+    cout << "What is your name? ";
+    getline (cin, fullName);
+    cout << "Well, hello, " + fullName << endl;
+
     cout << "Type first number: ";
     cin >> x;
     cout << "Type second number: ";
