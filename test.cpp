cc4a4fde2a804eda67adf696315971a4132e44a3
George Zakharov <george_zakharov@mail.ru>
2021-09-13 21:22:00 +0300

    Simple calculation

--- test.cpp
+++ test.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
-    cout << "I love C++";
+    int x, y, sum;
+    cout << "Hello User!" << endl;
+    cout << "Type first number: ";
+    cin >> x;
+    cout << "Type second number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "The sum is: " << sum;
     return 0;
 }
