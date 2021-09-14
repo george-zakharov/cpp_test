27e0e89c077136a8c93f59add8bc7cbc2a13da5e
George Zakharov <george_zakharov@mail.ru>
2021-09-14 22:55:43 +0300

    Minor update

--- test.cpp
+++ test.cpp
@@ -2,12 +2,22 @@
 #include <string>
 using namespace std;
 
+int sum;
+
+void addTwoNumbers(int x, int y) {
+    sum = x + y;
+    cout << "The sum is: " << sum;
+    return;
+}
+
 int main() {
-    int x, y, sum;
+    int x, y;
     string hello = "Hello User!";
     string fullName;
+    string* ptr = &hello; // Creating a pointer
 
     cout << hello << endl;
+    cout << ptr << endl;
     cout << "What is your name? ";
     getline (cin, fullName);
     cout << "Well, hello, " + fullName << endl;
@@ -16,7 +26,8 @@ int main() {
     cin >> x;
     cout << "Type second number: ";
     cin >> y;
-    sum = x + y;
-    cout << "The sum is: " << sum;
+    
+    addTwoNumbers(x, y);
+
     return 0;
 }
